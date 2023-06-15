e536a40dc6ca45520b7794a0080711821b9705f2
James Limbe <limbejay254@gmail.com>
2023-06-15 14:32:51 -0700

    Betty is now a close friend of mine

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints sring using put. 
- * Return : 0 for sucsess.
+ * Return: 0 (sucsess).
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints string using printf, then new line.
- * Return: 0 for success.
+ * Return: 0 (success).
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
