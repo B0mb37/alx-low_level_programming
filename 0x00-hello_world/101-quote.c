80f92db65cd0b34aee76696f386feefa087cf5f7
James Limbe <limbejay254@gmail.com>
2023-06-15 14:03:27 -0700

    Betty doesnt like me as much as checker does

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /*
- * main -  101_quote.
- * return : 1 for standard error.
+ * main - 101_quote.
+ * @printf: Print format.
+ * @return : 1 for standard error.
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Orpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
