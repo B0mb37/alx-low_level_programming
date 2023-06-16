638b2ac9a9222fc7e83feaaf0a92d5482f6b89a3
James Limbe <limbejay254@gmail.com>
2023-06-16 16:37:22 -0700

    Let us see the 100 perform

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,33 +12,20 @@ int main(void)
 	int i;
 	int j;
 	
-	
-	i = 48;
-	j = 49;
-	while (i < 57 && j < 58)
+	for (i = 0;i < 9; i++)
 	{
-		putchar(i);
-		putchar(j);
-		if (i == 56 && j == 57)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar("\n");
-			i++;
-			j++;
-		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				i++;
-				j = 1 + i;
-			}
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	
+	putchar('\n')
+	
 	return (0);
 }
