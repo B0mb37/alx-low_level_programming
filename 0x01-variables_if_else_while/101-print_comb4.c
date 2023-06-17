c64cb6d898efc2b906f71a1c0847904c971e11d0
James Limbe <limbejay254@gmail.com>
2023-06-16 18:25:43 -0700

    I had a nasty bug somewhere, unbelievable

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,12 +12,12 @@ int main(void)
 	int i;
 	int j;
 	int k;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j + 1; j < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
