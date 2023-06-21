b73fe1b076225c5a0fce1dd7c6a6e5d6a2eee34a
James Limbe <limbejay254@gmail.com>
2023-06-20 19:41:40 -0700

    Testing one two gmp

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <gmp.h>
 
 /**
  * main - finds and prints the first 98 fibonacci numbers from 1 & 2
@@ -7,16 +8,27 @@
 
 int main(void)
 {
-	unsigned long long int a = 1, b = 2;
-	int i, temp;
+	mpz_t a, b, temp;
+	int i;
+
+	mpz_init_set_ui(a, 1);
+	mpz_init_set_ui(b, 2);
+	mpz_init(temp);
+
+	printf("%s, ", mpz_get_str(NULL, 10, a));
 
 	for (i = 2; i <= 98; i++)
 	{
-		printf("%lli, ", a);
-		temp = a;
-		a = b;
-		b = temp + b;
+		mpz_swap(temp, a);
+		mpz_add(a, a, b);
+		mpz_swap(b, temp);
+		printf("%s, ", mpz_get_str(NULL, 10, a));
 	}
 	printf("\n");
+
+	mpz_clear(a);
+	mpz_clear(b);
+	mpz_clear(temp);
+
 	return (0);
 }
