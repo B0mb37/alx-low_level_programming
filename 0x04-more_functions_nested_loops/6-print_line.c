3398cd4a848c6fd1e81ac913ee9a5603111ecddd
James Limbe <limbejay254@gmail.com>
2023-06-22 18:18:14 -0700

    SIgn on the line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,18 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+	int i;
 
+	if (n > 0)
+	{
 		for (i = 0; i < n; i++)
 		{
 			_putchar("_");
 		}
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
