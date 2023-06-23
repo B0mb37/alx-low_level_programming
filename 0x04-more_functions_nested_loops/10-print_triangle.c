de5262240de3f73dc5c65bb9db9004190d2b51b5
James Limbe <limbejay254@gmail.com>
2023-06-23 06:58:23 -0700

    Waiting for the fall

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,9 +16,9 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= size - i; j++)
+			for (j = 1; j <= (size - i); j++)
 			{
 				_putchar(' ');
 			}
