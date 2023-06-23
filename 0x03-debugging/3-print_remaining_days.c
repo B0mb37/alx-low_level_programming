8dcfb0941ebea7480ef46c4ea671be7113e4ab51
James Limbe <limbejay254@gmail.com>
2023-06-23 04:50:39 -0700

    Who would I tell

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of year: %d\n", day + 1);
 			printf("Remaining days: %d\n", 366 - (day + 1));
 		}
-		else
+		else if (month < 2 || (month = 2 && day < 29))
 		{
 			printf("Day of year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
