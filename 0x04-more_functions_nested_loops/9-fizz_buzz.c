4fdafc91529c9b7761828c0d91400c9e36210d4e
James Limbe <limbejay254@gmail.com>
2023-06-22 19:20:41 -0700

    Committed to committing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,22 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- *fizz_buzz_print - print all the numbers between 1 and 100
+ * main - print all the numbers between 1 and 100
  * replace multiples of 3 with Fizz and those of 5 with Buzz
  * multiples of both replace with FizzBuzz
  */
 
-void fizz_buzz_print(void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 		}
@@ -30,4 +29,7 @@ void fizz_buzz_print(void)
 			printf("%d", i);
 		}
 	}
+	printf("\n");
+
+	return (0);
 }
