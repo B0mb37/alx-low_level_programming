1ce63d5d397414fd38e87f727665145d7868bc26
James Limbe <limbejay254@gmail.com>
2023-06-23 07:11:09 -0700

    The road less travelled is all I have known

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,20 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	else
+	
+	for (i = 0; i < n; i++)
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
