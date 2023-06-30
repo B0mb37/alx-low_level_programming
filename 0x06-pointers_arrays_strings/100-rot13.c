33611f0a748e058c1655660c7d0defda98af331b
James Limbe <limbejay254@gmail.com>
2023-06-30 12:03:21 -0700

    Sinc I started these, I guess lets see what I will get

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * rot13 - letter subscription cipher
+ * @n: string pointer passed into funct
+ * Return: n (encoded message)
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char normData[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotData[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == normData[j])
+			{
+				s[i] = rotData[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
