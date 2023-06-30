3e472a32ee18bf218bd23a1ff629526460f4567c
James Limbe <limbejay254@gmail.com>
2023-06-29 20:10:57 -0700

    Gaddamit batty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,21 +9,20 @@
 
 char *leet(char *s)
 {
-	char m[] = {97, 101, 108, 111, 116};
-	char n[] = {52, 51, 49, 48, 55};
+	char m[] = "aAeEoOtTIL";
+	char n[] = "4433007711";
 
 	int i, j;
 
-	i = 0;
-
-	while (s[i])
+	for (i = 0; i s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 5; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == c[j] || s[i] == *c(c + j) - 32;
-					s[i] = n[j];
+			if (s[i] == m[j])
+			{
+				s[i] = n[j];
+			}
 		}
-		i++;
 	}
 	return (s);
 }
