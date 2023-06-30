c08c00ba7c843348b2315a982b9ed6c55facf987
James Limbe <limbejay254@gmail.com>
2023-06-29 19:42:33 -0700

    REverse array sort

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,12 @@ void reverse_array(int *a, int n)
 	int start, finish, change;
 
 	start = 0;
-	finish = n -1;
-	while(start < finish)
+	finish = n - 1;
+	while (start < finish)
 	{
 		change = *(a + start);
-		*(a + begin) = *(a + finish)
-			*(a + finish) = change;
+		*(a + begin) = *(a + finish);
+		*(a + finish) = change;
 		start++;
 		finish--;
 	}
