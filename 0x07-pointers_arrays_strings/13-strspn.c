e1c3696c7ebaccdd083b5663899f31e60fd27472
James Limbe <limbejay254@gmail.com>
2023-07-03 13:52:51 -0700

    so sleepy, I hope code runs

---
+++ 0x07-pointers_arrays_strings/13-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn - funct that gets length of prefix substring
+ * @s: first value
+ * @accept: secong variable
+ * Return: value (bytes from s with bytes from accept)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, val, check;
+
+	val = 0;
+
+	for (i = 0; s[i] != '\0'; i ++)
+	{
+		check = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if accept[n] == s[i]
+			{
+				value ++;
+				check = 1;
+			}
+		}
+
+		if (check == 0)
+			return (val);
+	}
+
+	return (val);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - func that locates a character in a string
+ * @s: string to be checked
+ * @c: location to be chacked
+ * Return: s + i or NULL (pointer to first character of string or NULL)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
