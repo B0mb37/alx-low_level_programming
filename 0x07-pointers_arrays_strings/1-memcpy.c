b968e11062c7ae28402af7572e4986feefa35e2e
James Limbe <limbejay254@gmail.com>
2023-07-03 13:15:10 -0700

    Memory copied, had to

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - funct that copies memory area
+ * @dest: pointer to memory area copied to
+ * @src: pointer to memory area copied from
+ * @n:number of bytes to be copied
+ * Return: dest (pointer to memory area copied to)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0 i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
