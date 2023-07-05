a332df5eee5b1c911e55d0a46696e7924e4eec65
James Limbe <limbejay254@gmail.com>
2023-07-04 19:27:58 -0700

    Im in between broken moments

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - funct that sets value of a pointer to a char
+ * @s: pointer to pointer whose value is to be set
+ * @to: char pointer to be set as value of pointer
+ * Return: Always 0 (success)
+ */
+
+void set_string(char **s, cahr *to)
+{
+	*s = to;
+}
