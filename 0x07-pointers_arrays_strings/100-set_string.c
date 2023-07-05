394b0f607888bf26206feac8136acfd32b1ac9fe
James Limbe <limbejay254@gmail.com>
2023-07-04 19:36:09 -0700

    Why do we make the same mistaes over and over

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,10 +4,10 @@
  * set_string - funct that sets value of a pointer to a char
  * @s: pointer to pointer whose value is to be set
  * @to: char pointer to be set as value of pointer
- * Return: Always 0 (success)
+ * Return: void
  */
 
-void set_string(char **s, cahr *to)
+void set_string(char **s, char *to)
 {
 	*s = to;
 }
