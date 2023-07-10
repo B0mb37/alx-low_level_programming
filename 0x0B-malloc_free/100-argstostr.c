74365dabb7e29de9af228571a0da497022891f2d
James Limbe <limbejay254@gmail.com>
2023-07-10 15:03:33 -0700

    THese are the things that happen

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concatenates all arguments of program
+ * @ac: argument counts
+ * @av: argument vector pointer
+ * Return: ptr (new string pointer) or NULL (fails)
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int total_len = 0, i, j, k = 0;
+	char *ptr;
+
+	if (ac == NULL || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		total_len += strlen(av[i]) + 1;
+	ptr = malloc(total_len);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (j = 0; j < ac; j++)
+	{
+		strcpy(ptr + k, av[j]);
+		k += strlen(av[j]);
+		ptr[k++] = '\n';
+	}
+	ptr[k - 1] = '\0';
+
+	return (ptr);
+}
