e94e5268622d9212de7220c871deaa7c9b26fe5f
James Limbe <limbejay254@gmail.com>
2023-07-13 15:00:52 -0700

    What about this

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (new_size > old_size)
+	if (old_size > new_size)
 		new_size = old_size;
 	for (i = 0; i < new_size; i++)
 		new_ptr[i] = ((char *)ptr)[i];
