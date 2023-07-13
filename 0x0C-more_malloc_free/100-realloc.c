352bf28dbc0d3240cdd253bc7a09f4e5f9169c81
James Limbe <limbejay254@gmail.com>
2023-07-13 16:23:18 -0700

    debugging this is not nice

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	copy_size = old_size < new_size ? old_sixe : new_size;
+	copy_size = old_size < new_size ? old_size : new_size;
 
 	for (i = 0; i < copy_size; i++)
 		new_ptr[i] = ((char *)ptr)[i];
