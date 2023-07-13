bd2c229c6dcc46b1cecbdcc48bd529deba5fc7e4
James Limbe <limbejay254@gmail.com>
2023-07-13 14:43:33 -0700

    Freeway chronicles

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 	for (j = 0; i < l && s2[j]; i++, j++)
 		ptr[i] = s2[j];
-	str[i] = '\0';
+	ptr[i] = '\0';
 	return (ptr);
 }
