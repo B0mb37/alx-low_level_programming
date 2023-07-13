ec5db7ec1cceb1c42ad8604b488aefe4a769fde5
James Limbe <limbejay254@gmail.com>
2023-07-13 16:07:39 -0700

    Strugglesd ike into quotes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,10 +11,11 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
-	int i;
+	unsigned int i;
 
-	if (new_size == old_size)
+	if (new_size <= old_size)
 		return (ptr);
+
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
@@ -31,11 +32,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (old_size > new_size)
-		new_size = old_size;
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < old_size; i++)
 		new_ptr[i] = ((char *)ptr)[i];
+
 	free(ptr);
+
 	return (new_ptr);
 }
 
