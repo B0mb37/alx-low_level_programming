5b05bd8b098bf94fbfa852fb12aab14661c4a7c2
James Limbe <limbejay254@gmail.com>
2023-07-17 12:39:37 -0700

    I do not wanna wait

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,13 +5,14 @@
 /**
  * print_dog - Prints a struct dog
  * @d: dog structure pointer
+ * REturn: Nothing
  */
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d != ((void *)0))
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nill)");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 		printf("Age: %f\n", (d->name) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
