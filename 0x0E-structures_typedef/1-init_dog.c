8b1bd72b6118fce6ea4aa31c29495c6555796086
James Limbe <limbejay254@gmail.com>
2023-07-17 13:05:08 -0700

    Changed malloc from each to single struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,12 +14,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = malloc(strlen(name) + 1);
-		strcpy(d->name, name);
-
+		d = malloc(sizeof(struct dog));
+		d->name = name;
 		d->age = age;
-
-		d->owner = malloc(strlen(owner) + 1);
-		strcpy(d->owner, owner);
+		d->owner = owner;
 	}
 }
