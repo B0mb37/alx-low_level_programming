cbb4802ca62267373478dcdbd00ae5d05fe7a5b7
James Limbe <limbejay254@gmail.com>
2023-07-19 13:48:27 -0700

    I am initializing the function pointers code

---
+++ 0x0F-functions_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef	HUNCTIONS_H
+#define FUNCTIONS_H
+#include <stdlib.h>
+
+void print_name(char *name, void(*f)(char *));
+void arra_iteratior(int *array, size_t size, void(*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
