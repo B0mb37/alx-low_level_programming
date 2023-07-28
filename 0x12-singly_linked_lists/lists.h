77c58cad31cb1eecb4d9fb720a7b7fbaf6cebced
James Limbe <limbejay254@gmail.com>
2023-07-28 10:34:29 -0700

    Forgot to ad function prototype in lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,6 +27,7 @@ size_t print_list(const list_t *n);
 int _putchar(char c);
 int _strlen(char *str);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 char *_strdup(const char *str);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
