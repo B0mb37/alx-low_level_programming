7e19a6646ec24c530a39859616f24d4d037d5922
James Limbe <limbejay254@gmail.com>
2023-07-28 09:14:32 -0700

    Added missing function declarations

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,6 +23,13 @@ typedef struct list_s
 size_t power(size_t base, size_t power);
 size_t print_list(const list_t *n);
 void _putchar(char c);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
+
+
+
 
 
 #endif
