1b316fdbe32076bf9dceb16a44c140831f557c96
James Limbe <limbejay254@gmail.com>
2023-09-04 18:50:21 -0700

    appending text function update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,17 +9,22 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytesWritten;
+	ssize_t bytesWritten, len;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		bytesWritten = write(fd, text_content, strlen(text_content));
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	bytesWritten = write(fd, text_content, strlen(text_content));
 	close(fd);
-	if (bytesWritten == -1)
+	len = strlen(text_content);
+	if (bytesWritten != len)
 		return (-1);
-	return (-1);
+	return (1);
 }
