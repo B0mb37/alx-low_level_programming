7c7d79f542e0bf72980ecb136e25e9f2675936e7
James LImbe <limbejay254@gmail.com>
2023-11-17 18:37:52 -0800

    ISO C90 does not allow

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -34,12 +34,14 @@ void calcCoins(int cents, int *quaters, int *dimes, int *nickels, int *pennies)
 
 int main(int argc, char *argv[])
 {
+	int cents;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 		printf("0\n");
